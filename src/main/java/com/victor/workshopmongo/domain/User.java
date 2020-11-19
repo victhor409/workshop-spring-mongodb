7c75898ff1652bd6fd18fc4b479f489b0b50d903
victhor409 <victhor409@gmail.com>
2020-11-18 22:04:06 -0300

    ResposeEntity

---
+++ src/main/java/com/victor/workshopmongo/domain/User.java
@@ -0,0 +1,79 @@
+package com.victor.workshopmongo.domain;
+
+import java.io.Serializable;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class User implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	private String mail;
+	
+	public User() {
+		
+	}
+
+	public User(String id, String name, String mail) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.mail = mail;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+
+}
