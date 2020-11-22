c6fd0769a5b0c239dd0a8994356dc7b47e1d486d
victhor409 <victhor409@gmail.com>
2020-11-21 22:50:01 -0300

    Post inserção

--- src/main/java/com/victor/workshopmongo/services/UserService.java
+++ src/main/java/com/victor/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.victor.workshopmongo.domain.User;
+import com.victor.workshopmongo.dto.UserDTO;
 import com.victor.workshopmongo.repository.UserRepository;
 import com.victor.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,7 +25,14 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
-
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(),objDto.getMail());
+	}
 	
 }
 
