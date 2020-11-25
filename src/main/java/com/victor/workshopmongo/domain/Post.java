85e4600f11bd6e99b958c42dd8659a8a8e5d2ecd
victhor409 <victhor409@gmail.com>
2020-11-24 23:38:35 -0300

    salvando usuarios com AuthorDTO

--- src/main/java/com/victor/workshopmongo/domain/Post.java
+++ src/main/java/com/victor/workshopmongo/domain/Post.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.victor.workshopmongo.dto.AuthorDTO;
+
 @Document
 public class Post implements Serializable{
 	
@@ -17,13 +19,13 @@ public class Post implements Serializable{
 	private Date date;
 	private String title;
 	private String body;
-	private User author;
+	private AuthorDTO author;
 	
 	public Post() {
 		
 	}
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -66,11 +68,11 @@ public class Post implements Serializable{
 	
 	
 
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
